163b504731f3939174454d17e53e4e75ba45a1a0
msdeibel <git2018@markus-deibel.de>
2021-05-21 06:52:28 +0200

    Up to Login Redirect

--- save-points/00-get-started/BlazingPizza.Client/Program.cs
+++ save-points/00-get-started/BlazingPizza.Client/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
@@ -17,6 +18,9 @@ namespace BlazingPizza.Client
             builder.Services.AddScoped<OrderState>();
 
             builder.Services.AddApiAuthorization();
+
+            builder.Services.AddHttpClient<OrdersClient>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
             await builder.Build().RunAsync();
         }
